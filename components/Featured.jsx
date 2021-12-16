dcc50fd6c57f668f03fa80a243e678363b685861
Caesar <caesar.hinlo.tech@gmail.com>
2021-12-16 16:15:42 +0800

    save nav design for featured component

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -2,20 +2,23 @@ import React from "react";
 import Image from "next/image";
 import styles from "../styles/Featured.module.css";
 function Featured() {
-  const images = [
-    "/img/featured.png",
-    "/img/featured2.png",
-    "/img/featured3.png",
-  ];
+  const images = ["/img/featured.jpg", "/img/featured1.jpg"];
   return (
     <div className={styles.container}>
-      <Image src="/img/arrowl.png" alt="" layout="fill" />
+      <div className={styles.arrowContainer} style={{ left: 0 }}>
+        <Image src="/img/arrowl.png" alt="" layout="fill" />
+      </div>
+
       <div className={styles.imgContainer}>
         {images.map((img, i) => (
-          <Image src="/img/featured.png" key={i} alt="" layout="fill" />
+          <div className={styles.imgContainer} key={i}>
+            <Image src={img} alt="" layout="fill" objectFit="contain" />
+          </div>
         ))}
       </div>
-      <Image src="/img/arrowr.png" alt="" layout="fill" />
+      <div className={styles.arrowContainer} style={{ right: 0 }}>
+        <Image src="/img/arrowr.png" alt="" layout="fill" />
+      </div>
     </div>
   );
 }
