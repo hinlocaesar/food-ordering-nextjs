af998077c5550bb0eab0441787964950029cd201
Caesar <caesar.hinlo.tech@gmail.com>
2021-12-16 14:33:10 +0800

    Modification for navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import styles from "../styles/Navbar.module.css";
+import Image from "next/image";
 function Navbar() {
-  return <div>navbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src="/img/telephone.png" alt="" width="32" height="32" />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.texts}>Order Now!</div>
+          <div className={styles.texts}>09077449579</div>
+        </div>
+      </div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+    </div>
+  );
 }
 
 export default Navbar;
