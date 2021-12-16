2653ccbce705b5bb03398a5af6ff8df1040358e5
Caesar <caesar.hinlo.tech@gmail.com>
2021-12-16 16:30:56 +0800

    Save checkpoint

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import styles from "../styles/Featured.module.css";
 function Featured() {
-  const images = ["/img/featured.jpg", "/img/featured1.jpg"];
+  const [index, setIndex] = useState(0);
+  const images = [
+    "/img/featured.jpg",
+    "/img/featured1.jpg",
+    "/img/featured3.jpg",
+  ];
+
+  const handleArrow = (direction) => {
+    if (direction === "l") {
+      setIndex(index !== 0 ? index - 1 : 2);
+    }
+
+    if (direction === "r") {
+      setIndex(index !== 0 ? index + 1 : 0);
+    }
+  };
+
+  console.log(index);
   return (
     <div className={styles.container}>
-      <div className={styles.arrowContainer} style={{ left: 0 }}>
+      <div
+        className={styles.arrowContainer}
+        style={{ left: 0 }}
+        onClick={() => handleArrow("l")}
+      >
         <Image src="/img/arrowl.png" alt="" layout="fill" />
       </div>
 
@@ -16,7 +37,11 @@ function Featured() {
           </div>
         ))}
       </div>
-      <div className={styles.arrowContainer} style={{ right: 0 }}>
+      <div
+        className={styles.arrowContainer}
+        style={{ right: 0 }}
+        onClick={() => handleArrow("r")}
+      >
         <Image src="/img/arrowr.png" alt="" layout="fill" />
       </div>
     </div>
